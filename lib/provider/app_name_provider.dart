19a5bdd089e88fee89a765d3502362c63f05c924
Sho Emoto <dd2d00seg1@gmail.com>
2022-03-10 19:48:10 +0900

    autoDisposeを使って参照が無くなった時に自動破棄

--- lib/provider/app_name_provider.dart
+++ lib/provider/app_name_provider.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// アプリ名を提供する[Provider]を定義
-final appNameProvider = Provider((ref) => 'Riverpod Practice');
+final appNameProvider = Provider.autoDispose((ref) => 'Riverpod Practice');
 
 /// アプリ名を画面コンテンツに加工して提供する[Provider]を定義
-final appNameBodyProvider = Provider((ref) {
+final appNameBodyProvider = Provider.autoDispose((ref) {
   final appName = ref.watch(appNameProvider);
   return appName + ' Body Text';
 });
